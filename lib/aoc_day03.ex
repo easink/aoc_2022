7c8efbf7e824402f39c81e298752f42f03d10c93
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2022-12-04 22:35:32 +0100

    filename fixup

--- lib/aoc_day03.ex
+++ lib/aoc_day03.ex
@@ -1,48 +1,59 @@
-defmodule AoC.Day02 do
+defmodule AoC.Day03 do
   @moduledoc false
 
   def part1(filename) do
     filename
     |> parse()
-    |> Enum.map(&calc_round/1)
+    |> Enum.map(&parse_compartments/1)
+    |> Enum.map(&share_items/1)
+    |> Enum.map(&char_to_int/1)
     |> Enum.sum()
   end
 
   def part2(filename) do
     filename
     |> parse()
-    |> Enum.map(fn round ->
-      round
-      |> correction()
-      |> calc_round()
-    end)
+    |> Enum.chunk_every(3)
+    |> Enum.map(&share_items/1)
+    |> Enum.map(&char_to_int/1)
     |> Enum.sum()
   end
-
-  defp calc_round(["A", "X"]), do: 1 + 3
-  defp calc_round(["A", "Y"]), do: 2 + 6
-  defp calc_round(["A", "Z"]), do: 3 + 0
-  defp calc_round(["B", "X"]), do: 1 + 0
-  defp calc_round(["B", "Y"]), do: 2 + 3
-  defp calc_round(["B", "Z"]), do: 3 + 6
-  defp calc_round(["C", "X"]), do: 1 + 6
-  defp calc_round(["C", "Y"]), do: 2 + 0
-  defp calc_round(["C", "Z"]), do: 3 + 3
-
-  defp correction(["A", "X"]), do: ["A", "Z"]
-  defp correction(["A", "Y"]), do: ["A", "X"]
-  defp correction(["A", "Z"]), do: ["A", "Y"]
-  defp correction(["B", "X"]), do: ["B", "X"]
-  defp correction(["B", "Y"]), do: ["B", "Y"]
-  defp correction(["B", "Z"]), do: ["B", "Z"]
-  defp correction(["C", "X"]), do: ["C", "Y"]
-  defp correction(["C", "Y"]), do: ["C", "Z"]
-  defp correction(["C", "Z"]), do: ["C", "X"]
 
   defp parse(filename) do
     filename
     |> File.stream!()
     |> Enum.map(&String.trim/1)
-    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(&String.to_charlist/1)
   end
+
+  defp parse_compartments(line) do
+    n_items = length(line)
+
+    Enum.chunk_every(line, div(n_items, 2))
+  end
+
+  defp share_items([a, b]) do
+    a = MapSet.new(a)
+    b = MapSet.new(b)
+
+    [x] = MapSet.intersection(a, b) |> MapSet.to_list()
+    x
+  end
+
+  defp share_items([a, b, c]) do
+    a = MapSet.new(a)
+    b = MapSet.new(b)
+    c = MapSet.new(c)
+
+    [x] =
+      a
+      |> MapSet.intersection(b)
+      |> MapSet.intersection(c)
+      |> MapSet.to_list()
+
+    x
+  end
+
+  defp char_to_int(a) when a in ?a..?z, do: a - ?a + 1
+  defp char_to_int(a) when a in ?A..?Z, do: a - ?A + 27
 end
--- test/aoc_day01_test.exs
+++ test/aoc_day01_test.exs
@@ -8,6 +8,6 @@ defmodule AoCDay01Test do
 
   test "part2" do
     assert AoC.Day01.part2("inputs/day01.sample") == 45000
-    assert AoC.Day01.part2("inputs/day01") == 213958
+    assert AoC.Day01.part2("inputs/day01") == 213_958
   end
 end
